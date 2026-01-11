766311c2137b13e42f490bb6353a28fd8807bfa5
lacomas-a1 <lacomasjas03@gmail.com>
2026-01-12 01:28:46 +0300

    Gwira Summit

--- index.php
+++ index.php
@@ -73,7 +73,6 @@ $reviews = $reviews_stmt->fetchAll(PDO::FETCH_ASSOC);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Gwira Gwira Creatives Summit & Gala - Kilifi</title>
-    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"> -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <style>
         :root {
